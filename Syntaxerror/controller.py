0f61630bc6e0ffe94c59ecb50e495f85d57c7b7f
ManjotSingh08x <mithugunnu2006@gmail.com>
2024-10-19 00:25:59 +0530

    changed name

--- Syntaxerror/controller.py
+++ Syntaxerror/controller.py
@@ -20,9 +20,10 @@ blue = (0,0,255)
 
 import serial
 try:
-    ard = serial.Serial("/dev/ttyACM0", timeout=1)
+    ard = serial.Serial("COM5", timeout=1)
 except Exception as e:
     print(e)
+    exit()
 
 
 # Main game loop
